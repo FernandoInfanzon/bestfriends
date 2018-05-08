3e7e046471444ae3a50a1d077ed847035b605baa
Fernando Infanzon <luisfernandoim@gmail.com>
2018-05-08 10:25:26 -0500

    eliminar imagenes que no se usan

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(e) {
-    var o = new TimelineMax({ repeat: -1 });
-    window.onload = function() {
-        var e, n;
-        o.add((n = new TimelineMax({ repeat: -1 }), n.to("#ground", 100, { backgroundPosition: "1301px 0px", force3D: !0, rotation: .01, z: .01, autoRound: !1, ease: Linear.easeNone }), n), 0).add((e = new TimelineMax({ repeat: -1 }), e.to("#clouds", 52, { backgroundPosition: "-2247px bottom", force3D: !0, rotation: .01, z: .01, ease: Linear.easeNone }), e), 0).timeScale(.7).progress(1).progress(0).play()
-    }
-});
+// document.addEventListener("DOMContentLoaded", function(e) {
+//     var o = new TimelineMax({ repeat: -1 });
+//     window.onload = function() {
+//         var e, n;
+//         o.add((n = new TimelineMax({ repeat: -1 }), n.to("#ground", 100, { backgroundPosition: "1301px 0px", force3D: !0, rotation: .01, z: .01, autoRound: !1, ease: Linear.easeNone }), n), 0).add((e = new TimelineMax({ repeat: -1 }), e.to("#clouds", 52, { backgroundPosition: "-2247px bottom", force3D: !0, rotation: .01, z: .01, ease: Linear.easeNone }), e), 0).timeScale(.7).progress(1).progress(0).play()
+//     }
+// });
