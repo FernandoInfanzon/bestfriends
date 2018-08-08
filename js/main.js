a64d882d1d5dff4e6c9ec07d9724fdf5254e3a3d
Fernando Infanzon <luisfernandoim@gmail.com>
2018-08-08 12:42:03 -0500

    Se agregaron textos y boton en los modales

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log(e);
+    var o = new TimelineMax({ repeat: -1 });
+    window.onload = function() {
+        var e, n;
+        o.add((n = new TimelineMax({ repeat: -1 }), n.to("#ground", 100, { backgroundPosition: "1301px 0px", force3D: !0, rotation: .01, z: .01, autoRound: !1, ease: Linear.easeNone }), n), 0).add((e = new TimelineMax({ repeat: -1 }), e.to("#clouds", 52, { backgroundPosition: "-2247px bottom", force3D: !0, rotation: .01, z: .01, ease: Linear.easeNone }), e), 0).timeScale(.7).progress(1).progress(0).play()
+    }
+});
+
+
+$("#activarModalKinder").click(function() {
+    $("#modalKinder").modal("show");
+});
+
+
+$("#btnAgendarKinder").click(function() {
+    $("#modalKinder").modal("hide");
+});
+
+
+
+$("#activarModalMaternal").click(function() {
+    $("#modalMaternal").modal("show");
+});
+
+
+$("#btnAgendarMaternal").click(function() {
+    $("#modalMaternal").modal("hide");
+});
+
+
+
+
 $(document).ready(function() {
 
 
-    document.addEventListener("DOMContentLoaded", function(e) {
-        var o = new TimelineMax({ repeat: -1 });
-        window.onload = function() {
-            var e, n;
-            o.add((n = new TimelineMax({ repeat: -1 }), n.to("#ground", 100, { backgroundPosition: "1301px 0px", force3D: !0, rotation: .01, z: .01, autoRound: !1, ease: Linear.easeNone }), n), 0).add((e = new TimelineMax({ repeat: -1 }), e.to("#clouds", 52, { backgroundPosition: "-2247px bottom", force3D: !0, rotation: .01, z: .01, ease: Linear.easeNone }), e), 0).timeScale(.7).progress(1).progress(0).play()
-        }
-    });
 
+    console.log("ready");
 
 
 
